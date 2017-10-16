89f6dd792e480c2b2f254ac78b2308d2d02ac34d
spicydog <spicydog@gmail.com>
2017-10-17 00:32:38 +0700

    Make register support AJAX

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -67,5 +69,30 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        $this->guard()->login($user);
+
+        if($request->ajax()){
+            // If request from AJAX
+            return ['success' => true];
+        } else {
+            // Normal POST do redirect
+            return $this->registered($request, $user)
+                        ?: redirect($this->redirectPath());
+        }
+        
     }
 }
